2b8de09c6c336b8e855e35d4a655b26f3b3bed36
Anup Kodlekere <anup.kodlekere@ibm.com>
2022-05-17 17:15:18 +0530

    stupid mistake resolved

--- exec.sh
+++ exec.sh
@@ -1,2 +1,2 @@
-python3.8 script.py >> full_log.log
-cat full_log.log | grep "Worker information" -A4 -B5 | tee deploy_info.log
+python3.8 script.py
+cat deploy_info.log | grep "Worker information" -A4 -B5 | tee db_input
