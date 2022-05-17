b2e177d9be061476bde91b26f4fc93a44967710b
Anup Kodlekere <anup.kodlekere@ibm.com>
2022-05-17 15:55:41 +0530

    updated exec.sh

--- exec.sh
+++ exec.sh
@@ -1,2 +1,2 @@
-python3.8 script.py >> deploy_info.log
-cat deploy_info.log | grep "Worker information" -A4 -B5
+python3.8 script.py >> log.log
+cat log.log | grep "Worker information" -A4 -B5 > deploy_info.log 
